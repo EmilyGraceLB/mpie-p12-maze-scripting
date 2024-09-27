a4a4f84aaef4f9336e8d31f64fc47bea8da3399c
Emily <145925732+EmilyGraceLB@users.noreply.github.com>
2024-09-27 11:02:28 +0100

    maze wall script

--- Assets/MazeWallMove.cs
+++ Assets/MazeWallMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MazeWallMove : MonoBehaviour
 {
+    public float rotationSpeed = 50.0f;
+    public float speed = 50.0f;
+    public GameObject Maze;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class MazeWallMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+    
+        Maze.transform.rotation = Quaternion.Euler(translation, rotation, 0);
     }
 }
